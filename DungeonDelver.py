7ac15282b7dae76bb760b51bf71d6962a8d557f9
James Donkin <jamesdonkin@users.noreply.github.com>
2024-11-09 22:31:52 +0000

    Refactoring into Classes
    Extracted the weapon and character logic into separate files and classes and added a spear weapon.

--- DungeonDelver.py
+++ DungeonDelver.py
@@ -1,5 +1,4 @@
-import random
-from typing import Final
+import random, Weapons, Character
 
 monster_hp = 0
 ran_loot = 0
@@ -7,11 +6,6 @@ rock_has = 0
 encounter = 0
 spare_food = 0
 monster_dm = 0
-
-class WeaponClass:
-    BOW: Final[int] = 1
-    SWORD: Final[int] = 2
-    AXE: Final[int] = 3
 
 
 def kick_down_door():
@@ -66,7 +60,7 @@ def kick_down_door():
             monster_hp = 7
             monster_dm = 2
             monster_attack()
-            if health <= 0:
+            if character.health <= 0:
                 print("you are eaten by the wolf pack")
                 break
         looting()
@@ -103,14 +97,14 @@ def kick_down_door():
             monster_dm = 1
             monster_name = "the very skinny goblin"
             monster_attack()
-            if health <= 0:
+            if character.is_dead():
                 print("the skinny goblin smiles with glee")
                 return()
             monster_hp = 7
             monster_dm = 3
             monster_name = "the tall lanky goblin who knows karate"
             monster_attack()
-            if health <= 0:
+            if character.is_dead():
                 print("the lanky goblin shadowboxes on your corpse")
                 return()
         monster_hp = 50
@@ -125,7 +119,7 @@ def kick_down_door():
         monster_dm = 4
         monster_name = "the red dragon"
         monster_attack()
-        if health <= 0:
+        if character.is_dead():
             return ()
         print("The dragon releases out a thunderous roar")
         monster_hp = 30
@@ -139,21 +133,21 @@ def kick_down_door():
         monster_dm = 3
         monster_name = "the lich"
         monster_attack()
-        if health <= 0:
+        if character.is_dead():
             print("The lich revives you as an undead servant")
             return ()
         print("the lich backs away, sending his undead minions instead")
         monster_hp = 20
         monster_dm = 1
         monster_name = "the zombie"
-        if health <= 0:
+        if character.is_dead():
             print("The lich revives you as an undead servant")
             return ()
         monster_attack()
         monster_hp = 8
         monster_dm = 3
         monster_name = "the sceleton"
-        if health <= 0:
+        if character.is_dead():
             print("The lich revives you as an undead servant")
             return ()
         print("the lich returns to the battle, though he is still injured from earlier")
@@ -182,7 +176,7 @@ def kick_down_door():
             monster_hp = 100
             monster_dm = 15
         monster_attack()
-        if health > 0:
+        if character.is_alive():
             print("did you just kill goku? Uh you win I guess, nice job!")
         else:
             return()
@@ -190,8 +184,8 @@ def kick_down_door():
 
 
 def looting():
-    global ran_loot, health, weapon_dur, rock_has, spare_food, weapon_dam
-    if health >= 0:
+    global ran_loot, rock_has, spare_food
+    if character.is_alive():
         print("You search the room")
     else:
         return()
@@ -199,77 +193,77 @@ def looting():
     # give me liberty, give me fire, give me if statements, or I retire
     if ran_loot == 2:
         print("you find a moldy piece of bread, you attempt to cut of the mold and heal 1 health")
-        health = health + 1
+        character.heal(1)
     elif ran_loot == 3:
         print("you find some rats, you cook them and eat them healing 3 health.")
-        health = health + 3
+        character.heal(3)
     elif ran_loot == 4:
         print("you find a health potion, it heals you 8 health")
-        health = health + 8
+        character.heal(8)
     elif ran_loot == 5:
         print("you find a rusty buckler, that can take a parry instead of your weapon twice")
-        weapon_dur = weapon_dur + 2
+        character.weapon.enhance_durability(2)
         y = random.randint(0, 2)
         if y == 1:
             print("you also find a boiled egg that heals you 3 health")
-            health = health + 3
+            character.heal(3)
         elif y == 2:
             print("you also find a moldy piece of bread, you attempt to cut of the mold and heal 1 health")
-            health = health + 1
+            character.heal(1)
     elif ran_loot == 6:
         print("you find a general potion, it heals you 5 health and repairs your weapon slightly")
-        health = health + 5
-        weapon_dur = weapon_dur + 2
+        character.heal(5)
+        character.weapon.enhance_durability(2)
     elif ran_loot == 7:
         print("you find a greater healing potion, it heals you 10 health")
-        health = health + 10
+        character.heal(10)
     elif ran_loot == 8:
         print("you find an iron buckler, it can parry instead of your weapon 4 times")
-        weapon_dur = weapon_dur + 4
+        character.weapon.enhance_durability(4)
         y = random.randint(0, 2)
         if y == 1:
             print("you also find a piece of bread that heals you 4 health")
-            health = health + 4
+            character.heal(4)
         elif y == 2:
             print("you also find a steak that heals you 6 health")
-            health = health + 6
+            character.heal(6)
     elif ran_loot == 9 or ran_loot == 10:
         if rock_has == 0:
             print("you find an enchanted rock, could come in handy")
             rock_has = 1
         print("your rock glows, you heal 12")
-        health = health + 12
+        character.heal(12)
     elif ran_loot == 11:
         print("you find a goblin feast, you heal 15 and gain some spare food")
-        health = health + 15
+        character.heal(15)
         spare_food = 1
     elif ran_loot == 12:
         print("you find an armoury, and take a side dagger boosting your damage")
-        weapon_dam = weapon_dam + 2
-        weapon_dur = weapon_dur + 2
+        character.weapon.enhance_damage(2)
+        character.weapon.enhance_durability(2)
     elif ran_loot == 13:
         print("you find a divine potion, healing you 20 and restoring your weapon massively")
-        health = health + 20
-        weapon_dur = weapon_dur + 4
+        character.heal(20)
+        character.weapon.enhance_durability(4)
     elif ran_loot == 14:
         if rock_has == 0:
             print("you find an enchanted rock, could come in handy")
             rock_has = 1
         print("your rock glows, your damage increases and you heal 5")
-        weapon_dam = weapon_dam + 2
-        health = health + 5
+        character.weapon.enhance_damage(2)
+        character.heal(5)
     elif ran_loot == 15:
         print("you find a pantry containing fresh food, you heal 20 and gain some spare food")
-        health = health + 20
+        character.heal(20)
         spare_food = 1
     else:
         print("you find nothing")
 
 
 def monster_attack():
-    global monster_hp, health, weapon_dam, weapon_dur
+    global monster_hp
     print("you encounter", monster_name)
-    while monster_hp > 0 and health > 0:
+    while monster_hp > 0 and character.is_alive():
         incoming = random.randint(-2, 4) + monster_dm
         if incoming < 0:
             incoming = 0
@@ -280,88 +274,70 @@ def monster_attack():
             incoming = incoming-3
             if incoming < 0:
                 incoming = 0
-            health = health - incoming
-            print("you have", health, "health remaining")
+            character.injure(incoming)
         elif fight_act == "2":
             print("you go to strike the enemy")
-            print("your damage is", weapon_dam)
-            monster_hp = monster_hp-weapon_dam
-            health = health - incoming
-            print("you have", health, "health remaining")
+            print("your damage is", character.weapon.damage)
+            monster_hp = monster_hp - character.weapon.damage
+            character.injure(incoming)
         elif fight_act == "3":
-            weapon_dur = weapon_dur-1
-            if weapon_dur <= 0 and weapon_dam == 1:
-                weapon_dur = 0
-                health = health - incoming
+            print("You try to parry with your", character.weapon.name())
+
+            if isinstance(character.weapon, Weapons.Unarmed):
                 print("you try and parry with your fists, It has no effect")
-                print("you have", health, "health remaining")
-            elif weapon_dur <= 0:
-                weapon_dur = 0
-                print("you parry but your weapon shatters!")
-                weapon_dam = 1
+                character.injure(incoming)
             else:
-                print("you parry but your weapon is damaged")
-                print("your weapon is", weapon_dur, "hits away from being destroyed")
+                character.weapon.damaged(1)
+                if character.weapon.durability <= 0:
+                    print("you parry but your " + character.weapon.name() + " shatters!")
+                    character.weapon = Weapons.Unarmed()
+                else:
+                    print("you parry but your " + character.weapon.name() + " is damaged")
+                    print("your weapon is", character.weapon.durability, "hits away from being destroyed")
         else:
             print("you miss your opportunity to act")
-            health = health - incoming
-            print("you have", health, "health remaining")
-    if health > 0:
+            character.injure(incoming)
+    if character.is_alive():
         print("You win!")
     else:
         print("Your dead")
         return()
 
-
 running = 1
-
-def describe_entering_dungeon():
-    match weapon:
-        case WeaponClass.BOW:
-            print("You enter the dungeon with 30 health and walk into the first room, bow gripped tight in your hand")
-        case WeaponClass.SWORD:
-            print("You enter the dungeon with 30 health and walk into the first room, your trusty blade held tight")
-        case WeaponClass.AXE:
-            print("You enter the dungeon with 30 health and walk into the first room, gripping the handle of your axe")
-        case _:
-            print("You enter the dungeon and with 30 health walk into the first room, shaking as you walk unarmed")
-
-
 while running == 1:
+
+    character = Character.Character()
     monster_hp = 0
     ran_loot = 0
     rock_has = 0
     encounter = 0
     spare_food = 0
     monster_dm = 0
-    health = 30
     monster_name = 0
 
-    weapon = int(input("please select your starting weapon, 1 for a bow, 2 for a sword, 3 for an axe"))
+    weaponChoice = int(input("please select your starting weapon,"
+                             " 1 for a bow, 2 for a sword, 3 for an axe, 4 for a spear"))
+    match weaponChoice:
+        case Weapons.WeaponTypes.AXE:
+            character.weapon = Weapons.Axe()
+        case Weapons.WeaponTypes.SWORD:
+            character.weapon = Weapons.Sword()
+        case Weapons.WeaponTypes.BOW:
+            character.weapon = Weapons.Bow()
+        case Weapons.WeaponTypes.SPEAR:
+            character.weapon = Weapons.Spear()
 
-    # selects starting weapon, and is stored as an integer
-    if weapon == WeaponClass.AXE:
-        weapon_dur = random.randint(4, 6)
-        weapon_dam = 5
-    elif weapon == WeaponClass.SWORD:
-        weapon_dur = random.randint(7, 9)
-        weapon_dam = 4
-    elif weapon == WeaponClass.BOW:
-        weapon_dur = random.randint(2, 4)
-        weapon_dam = 6
-    else:   # what kind of weapon is this???
-        weapon_dur = 0
-        weapon_dam = 1
-
-    describe_entering_dungeon()
+    # Welcome to the dungeon message
+    print("You enter the dungeon with " + format(character.health) +
+          " health and walk into the first room, " + character.weapon.active_description())
 
     # changing starting message to show what weapon you have picked
     for x in range(5):
         encounter = encounter + random.randint(1, 3)
         kick_down_door()
-        if health <= 0:
+        if character.is_dead():
             break
-    if health <= 0:
+    if character.is_dead():
         running = int(input("continue? 1 for yes 2 for no"))
     else:
         encounter = 100

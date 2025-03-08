272c7105bcb34d0c166681ed804caeca8ca64265
User1035 <TorDonkin@gmail.com>
2025-03-08 17:51:26 +0000

    Sigma

--- Monsters.py
+++ Monsters.py
@@ -7,22 +7,27 @@ class Monster:
         self.monster_hp = 0
         self.monster_dm = 0
 
-    def name(self):
-        return "Monster"
-
     def damaged(self, amount_of_damage):
         self.monster_hp = self.monster_hp - amount_of_damage
+
+
+class GiantSpider(Monster):
+    def __init__(self):
+        self.monster_hp = 4
+        self.monster_dm = 3
+        self.name = "The giant spider"
+
+
 
 class Rat(Monster):
     def __init__(self):
         self.monster_hp = 8
         self.monster_dm = 1
-    def name(self):
-        return "The giant rat"
+        self.name = "The giant rat"
 
-class Fatgoblin(Monster):
+
+class FatGoblin(Monster):
     def __init__(self):
         self.monster_hp = 8
         self.monster_dm = 1
-    def name(self):
-        return "The fat goblin"
+        self.name = "The fat goblin"
